7eac51640070b18ca950804dbca096ac195b1609
Kiryushin Denis <dkiryushin@sedmax.com>
2020-10-03 11:40:22 +0300

    fork registry

---
+++ nats_registry/changed.go
@@ -0,0 +1,39 @@
+package nats
+
+import (
+	"github.com/slonegd-otus-go/registry_v1" // change
+	"github.com/micro/go-micro/v2/registry"
+)
+
+func cp(current []*registry_v1.Service) []*registry.Service {
+	var services []*registry.Service
+
+	for _, service := range current {
+		// copy service
+		s := new(registry.Service)
+		*s = *service
+
+		// copy nodes
+		var nodes []*registry.Node
+		for _, node := range service.Nodes {
+			n := new(registry.Node)
+			*n = *node
+			nodes = append(nodes, n)
+		}
+		s.Nodes = nodes
+
+		// copy endpoints
+		var eps []*registry.Endpoint
+		for _, ep := range service.Endpoints {
+			e := new(registry.Endpoint)
+			*e = *ep
+			eps = append(eps, e)
+		}
+		s.Endpoints = eps
+
+		// append service
+		services = append(services, s)
+	}
+
+	return services
+}

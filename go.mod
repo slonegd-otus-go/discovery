0d2d56367b168922eb7c4a3796400553248ecfee
Kiryushin Denis <dkiryushin@sedmax.com>
2020-10-03 13:53:50 +0300

    send signal via micro

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/micro/go-micro/v2 v2.9.1
+	github.com/micro/go-plugins/client/http/v2 v2.9.1
 	github.com/nats-io/nats.go v1.10.0
 )
--- update_signal/main.go
+++ update_signal/main.go
@@ -1,20 +1,21 @@
 package main
 
-
-
 import (
+	"context"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/client/selector"
 	"github.com/micro/go-micro/v2/registry"
-	"github.com/slonegd-otus-go/discovery/registry/nats"
+	http_client "github.com/micro/go-plugins/client/http/v2"
 
 	"github.com/slonegd-otus-go/discovery"
+	"github.com/slonegd-otus-go/discovery/registry/nats"
 )
 
-func main()  {
+func main() {
 	registry := nats.NewRegistry(registry.Addrs("nats://127.0.0.1:4222")) // TODO вынести в глобалку/флаги
 	selector := selector.NewSelector(selector.Registry(registry))
 	discovery := discovery.New(selector)
@@ -24,11 +25,29 @@ func main()  {
 	}
 	log.Println("address of sedmax.web.signals:", address)
 
-	response, err := http.Get("http://"+address + "/update/configuration")
+	log.Printf("send update configuration signal via http")
+
+	response, err := http.Get("http://" + address + "/update/configuration")
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("response: %+v", response)
 
-	log.Printf("send update configuration signal, response: %+v", response)
+	log.Printf("send update configuration signal via micro")
+
+	service := micro.NewService(
+		micro.Name("test.update_signal"),
+		micro.Registry(registry),
+		micro.Client(http_client.NewClient()),
+	)
+
+	request := service.Client().NewRequest("sedmax.web.signals", "/update/configuration", nil)
+	var microResponse interface{}
+	err = service.Client().Call(context.Background(), request, &microResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("response: %+v", microResponse)
+
 	time.Sleep(time.Second)
 }

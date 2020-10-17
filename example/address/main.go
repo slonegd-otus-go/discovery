d596efcd7fcffdd3ada4ebb714414a88d8bf8b90
Kiryushin Denis <dkiryushin@sedmax.com>
2020-10-17 08:54:45 +0300

    address example

---
+++ example/address/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/slonegd-otus-go/discovery/example/discovery"
+	nats "github.com/slonegd-otus-go/discovery"
+
+	"github.com/micro/go-micro/v2/client/selector"
+	"github.com/micro/go-micro/v2/registry"
+)
+
+func main() {
+	service := "sedmax.controller"
+	if len(os.Args) > 1 {
+		service = os.Args[1]
+	}
+	registry := nats.NewRegistry(registry.Addrs("nats://127.0.0.1:4222"))
+	selector := selector.NewSelector(selector.Registry(registry))
+	discovery := discovery.New(selector)
+
+	address, err := discovery.GetAddress(service)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf(address)
+}
